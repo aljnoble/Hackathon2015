1da25c2604258087b8a617c11fd21523b20c0de1
Alec <aljnoble@ucsc.edu>
2015-09-26 14:49:34 -0700

    THE PROJECT FORMERLY KNOWN AS CODEPALS

--- NetflixAndCode.js
+++ NetflixAndCode.js
@@ -4,9 +4,11 @@ var sinchClient = new SinchClient({
 	capabilities: {messaging: true, calling: true},
 	startActiveConnection: true,
 	// The next three lines are for more verbose debugging
+
 	//onLogMessage: function(message) {
 	//	console.log(message);
 	//}
+
 });
 
 var sessionName = 'CodeButts-' + sinchClient.applicationKey;
